fe26a9b9f872f1943c3643a135af4659ed617237
AmandaCascales <acv41@gcloud.ua.es>
2026-03-23 23:50:27 +0100

    rotatición personaje y quitar el movimiento

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensX = 0f;
+    [SerializeField]
+    private float _sensY = 0f;
+    // Dirección del movimiento
+    private float xRotation;
+    private float yRotation;
+    [SerializeField]
+    private Transform _orientation;
+    private Vector2 _lookInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = _lookInput.x * Time.deltaTime * _sensX;
+        float mouseY = _lookInput.y * Time.deltaTime * _sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        _orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+    public void SetLookInput(Vector2 lookInput)
+    {
+        _lookInput = lookInput;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Camera Settings")]
     private Camera _camera;
-    private float _horizontal = 0f;
-    private float _vertical = 0f;
-    // Dirección del movimiento
-    private Vector3 _direction;
+   
     public bool showGizmos = false;
-
-    // Velocidad deseada en base a la dirección y la velocidad máxima
-    private Vector3 _desiredVelocity;
-    [Header("Player Movement")]
-    [SerializeField]
-    private float _movementSpeed = 8f;
-    [SerializeField]
-    private float _acceleration = 30f;
-
     [Header("Collision pre detection")]
     [SerializeField]
     private LayerMask _checkLayer;
@@ -26,10 +15,6 @@ public class PlayerController : MonoBehaviour
     private Transform _checkPoint;
     [SerializeField]
     private float _checkSize = 0.3f;
-    [Range(0,3)]
-    [SerializeField]
-    private float _checkDistance = 2f;
-    private bool _isWalled;
     [Header("Physics")]
     [SerializeField]
     private Rigidbody _rb;
@@ -50,8 +35,6 @@ public class PlayerController : MonoBehaviour
     private LayerMask _pointerLayer;
     [SerializeField]
     private Transform _aimingPivot;
-    [SerializeField]
-    private bool _isAiming=false;
 
     void Start()
     {
@@ -60,71 +43,18 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GroundCheck();
-        CollisionPreDetection();
-        Movement();
-        Aiming();
     }
     #region Methods
     /// <summary>
     /// Métodos nuevos del system input actualizado
     /// </summary>
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        Vector2 input = context.ReadValue<Vector2>();
-        _horizontal = input.x;
-        _vertical = input.y;
-    }
     public void OnLook(InputAction.CallbackContext context)
     {
         _lookInput = context.ReadValue<Vector2>();
         if(_lookInput.magnitude < 0.5f) 
             _lookInput = Vector2.zero;
+        _camera.GetComponent<PlayerCamera>().SetLookInput(_lookInput);
         _usingGamepad = context.control.device is Gamepad;
-    }
-    private void Movement()
-    {
-        // Componemos el vector de dirección deseado a partir del input
-        _direction.Set(_horizontal, 0f, _vertical);
-        // Para asegurarnos de que las diagonales no tienen una magnitud mayor a 1, 
-        // limitamos la magnitud del vector.
-        _direction = Vector3.ClampMagnitude(_direction, 1f);
-        // Calculamos la velocidad deseada en base a la dirección y la velocidad
-        _desiredVelocity = _direction * _movementSpeed;
-
-        // Sitúa el checker de colisión en la dirección a la que deseamos mover el tanque
-        // haciendo uso de un vector 3 temporal para respetar la altura configurada en el objeto vacío
-        Vector3 temp = transform.position + _direction * _checkDistance;
-        temp.y = _checkPoint.position.y;
-        _checkPoint.position = temp;
-        // Si hay colisión paramos el movmiento
-        if (_isWalled)
-        {
-            _desiredVelocity = Vector3.zero;
-        }
-        // Solo aplicamos y rotamos al jugador en caso de cumplir las siguientes condiciones:
-        // - Que haya input
-        // - Que no haya detección de colisión frontal
-        // - Que estemos tocando el suelo
-        if((_horizontal != 0f || _vertical != 0f) && !_isWalled && _grounded)
-        {
-            // Aplicamos el movimiento
-            _rb.linearVelocity = Vector3.MoveTowards(_rb.linearVelocity,
-                                                    _desiredVelocity,
-                                                    Time.deltaTime * _acceleration);
-        } 
-    }
-    /// <summary>
-    ///  Detecta si hay un collider del layer indicado en contacto con el checker de 
-    /// colisión frontal ( de desplazamiento).
-    /// </summary>
-    private void CollisionPreDetection()
-    {
-        Collider[] colliderBuffered = new Collider[1];
-        Physics.OverlapSphereNonAlloc(_checkPoint.position,
-                                    _checkSize,
-                                    colliderBuffered,
-                                    _checkLayer);
-        _isWalled = colliderBuffered[0] != null;
     }
     /// <summary>
     /// Comprueba si está en contacto con el suelo y actualiza la booleana _grounded.
@@ -156,40 +86,6 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.red;
         // Mostramos el Gizmo del ground check pintando un cubo
         Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
-    }
-    private void Aiming() 
-    {
-        if (_usingGamepad)
-        {
-            AimingWithStick();
-        }
-        else
-        {
-            AimingWithMouse();
-        }
-    }
-
-    private void AimingWithMouse()
-    {
-        if(Mouse.current == null) return;
-        Vector2 mousePos = Mouse.current.position.ReadValue();
-        Ray ray = _camera.ScreenPointToRay(mousePos);
-        if (Physics.Raycast(ray, out RaycastHit hit, _camRayLenght, _pointerLayer))
-        {
-            _aimingPivot.position = new Vector3(hit.point.x, _aimingPivot.position.y, hit.point.z);
-        }
-    }
-
-    private void AimingWithStick()
-    {
-        Vector3 direction = new Vector3(_lookInput.x, 0f, _lookInput.y);
-        
-        if(direction.sqrMagnitude < 0.01f) return;
-        direction.Normalize();
-
-        Vector3 target = transform.position + direction;
-
-        _aimingPivot.position = new Vector3(target.x, _aimingPivot.position.y, target.z);
     }
     #endregion
 }
